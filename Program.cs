a676284ba15ba0ebe1b57ad5f335f762f3e35605
Koitchev,Ivan I.G. <ivan@appsemble.com>
2024-10-24 18:14:23 +0300

    Initial

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+class Program
+{
+    private static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+        int M = Convert.ToInt32(Console.ReadLine());
+        
+        int nrIslands = 0;
+
+        if (N < 3 || M < 3) {
+            Console.WriteLine(nrIslands);
+            return;
+        }   
+        int[,] matrix = new int[N, M];
+
+        // Input matrix one number at a time
+        for (int i = 0; i < M; i++){
+            for (int j = 0; j < N; j++){
+                matrix [j,i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        
+
+    }
+}
